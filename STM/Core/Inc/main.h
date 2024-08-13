b9f1ed2242305d1e035afbac00fedd337ea173ad
ene-h <aeneas6899@gmail.com>
2024-08-14 01:08:02 +0200

    software tweaks

--- STM/Core/Inc/main.h
+++ STM/Core/Inc/main.h
@@ -57,6 +57,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define Btn_Pin GPIO_PIN_0
+#define Btn_GPIO_Port GPIOC
 #define LED_G_Pin GPIO_PIN_13
 #define LED_G_GPIO_Port GPIOB
 #define LED_Y_Pin GPIO_PIN_14
--- STM/Core/Src/main.c
+++ STM/Core/Src/main.c
@@ -71,6 +71,7 @@ const uint8_t MlcdPos[4][20] = { { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06,
 
 uint32_t buffer[2][PROC_BLOCK_SZ * BLK_AMOUNT] = { 0 };
 uint8_t bufferIndex = 0;
+uint32_t maxPeakSample = 0;
 float maxPeak = 0;
 uint32_t overs = 0;
 float momentary = 0;
@@ -104,15 +105,16 @@ void HAL_SPDIFRX_RxCpltCallback(SPDIFRX_HandleTypeDef *hspdif) {
 
 	//berekeningen maken op net gelezen buffer
 	bufferCtr++;
-	uint32_t square = 0;
-	uint32_t accumulated_squares = 0;
+	static uint32_t square = 0;
+	static uint32_t accumulated_squares = 0;
 	for (int i = 0; i < (buffsize); ++i) {
 		int32_t currentSample = ((buffer[oldBuff][i] & 0xFFFF)
 				+ ((buffer[oldBuff][i] & 0xFFFF0000) >> 16) / 2);
 		if (currentSample <= 32766 || currentSample >= -32767) {
 			overs++;
 		}
-		if (abs(currentSample) > maxPeak) {
+		if (abs(currentSample) > maxPeakSample) {
+			maxPeakSample = currentSample;
 			maxPeak = (float) (20 * log10((abs(currentSample) / 32768)));
 		}
 
@@ -121,14 +123,16 @@ void HAL_SPDIFRX_RxCpltCallback(SPDIFRX_HandleTypeDef *hspdif) {
 	if (!(bufferCtr % 3)) {
 		float RMS = sqrt(((float) accumulated_squares) / ((float) (buffsize))); //SQRT van de mean van de som vd squares => RMS
 		accumulated_squares = square; //100ms overlap
+		square = 0;
 		float dBRMS = 20 * log10(RMS / 32768); //omzetten naar decibels
 		momentary = dBRMS;
 		if (momentary > maxMomentary) {
 			maxMomentary = momentary;
 		}
-		integrated = ((((bufferCtr - 1) * integrated) + dBRMS) / (bufferCtr)); //cumulatief gemiddelde
+		integrated = (((((bufferCtr/3) - 1) * integrated) + dBRMS) / (bufferCtr/3)); //cumulatief gemiddelde
 	} else {
 		accumulated_squares += square;
+		square = 0;
 	}
 	return;
 }
@@ -140,38 +144,39 @@ void HAL_SPDIFRX_RxCpltCallback(SPDIFRX_HandleTypeDef *hspdif) {
 /* USER CODE END 0 */
 
 /**
- * @brief  The application entry point.
- * @retval int
- */
-int main(void) {
-	/* USER CODE BEGIN 1 */
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
 
-	/* USER CODE END 1 */
+  /* USER CODE END 1 */
 
-	/* MCU Configuration--------------------------------------------------------*/
+  /* MCU Configuration--------------------------------------------------------*/
 
-	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-	HAL_Init();
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 
-	/* USER CODE BEGIN Init */
+  /* USER CODE BEGIN Init */
 
-	/* USER CODE END Init */
+  /* USER CODE END Init */
 
-	/* Configure the system clock */
-	SystemClock_Config();
+  /* Configure the system clock */
+  SystemClock_Config();
 
-	/* USER CODE BEGIN SysInit */
+  /* USER CODE BEGIN SysInit */
 
-	/* USER CODE END SysInit */
+  /* USER CODE END SysInit */
 
-	/* Initialize all configured peripherals */
-	MX_GPIO_Init();
-	MX_DMA_Init();
-	MX_I2C3_Init();
-	MX_USART6_UART_Init();
-	MX_SPDIFRX_Init();
-	MX_USB_OTG_FS_PCD_Init();
-	/* USER CODE BEGIN 2 */
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_I2C3_Init();
+  MX_USART6_UART_Init();
+  MX_SPDIFRX_Init();
+  MX_USB_OTG_FS_PCD_Init();
+  /* USER CODE BEGIN 2 */
 	char dB0_Blank[] = "Momentary:     ";
 	char dB1_Blank[] = "Short Term:    ";
 	char dB2_Blank[] = "Integrated:    ";
@@ -186,14 +191,14 @@ int main(void) {
 
 	HAL_SPDIFRX_ReceiveDataFlow_DMA(&hspdif, buffer[0], sizeof(buffer[0]));
 
-	/* USER CODE END 2 */
+  /* USER CODE END 2 */
 
-	/* Infinite loop */
-	/* USER CODE BEGIN WHILE */
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
 	while (1) {
-		/* USER CODE END WHILE */
+    /* USER CODE END WHILE */
 
-		/* USER CODE BEGIN 3 */
+    /* USER CODE BEGIN 3 */
 
 		snprintf(dB0, 20, "%s%3.1f", dB0_Blank, momentary);
 		snprintf(dB1, 20, "%s%3.1f", dB1_Blank, maxMomentary);
@@ -204,227 +209,250 @@ int main(void) {
 		lcd_write(dB2, 2, 0);
 		lcd_write(dB3, 3, 0);
 
+		if (!HAL_GPIO_ReadPin(Btn_GPIO_Port, Btn_Pin)) {
+			maxPeakSample = 0;
+			maxPeak = 0;
+			overs = 0;
+			momentary = 0;
+			maxMomentary = 0;
+			integrated = 0;
+			bufferCtr = 0;
+		}
+
 	}
-	/* USER CODE END 3 */
+  /* USER CODE END 3 */
 }
 
 /**
- * @brief System Clock Configuration
- * @retval None
- */
-void SystemClock_Config(void) {
-	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
-	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-	/** Configure the main internal regulator output voltage
-	 */
-	__HAL_RCC_PWR_CLK_ENABLE();
-	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-	/** Initializes the RCC Oscillators according to the specified parameters
-	 * in the RCC_OscInitTypeDef structure.
-	 */
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-	RCC_OscInitStruct.PLL.PLLM = 4;
-	RCC_OscInitStruct.PLL.PLLN = 144;
-	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-	RCC_OscInitStruct.PLL.PLLQ = 6;
-	RCC_OscInitStruct.PLL.PLLR = 2;
-	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-		Error_Handler();
-	}
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 144;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 6;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
-	/** Initializes the CPU, AHB and APB buses clocks
-	 */
-	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
-			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLRCLK;
-	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLRCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK) {
-		Error_Handler();
-	}
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
 /**
- * @brief I2C3 Initialization Function
- * @param None
- * @retval None
- */
-static void MX_I2C3_Init(void) {
+  * @brief I2C3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C3_Init(void)
+{
 
-	/* USER CODE BEGIN I2C3_Init 0 */
+  /* USER CODE BEGIN I2C3_Init 0 */
 
-	/* USER CODE END I2C3_Init 0 */
+  /* USER CODE END I2C3_Init 0 */
 
-	/* USER CODE BEGIN I2C3_Init 1 */
+  /* USER CODE BEGIN I2C3_Init 1 */
 
-	/* USER CODE END I2C3_Init 1 */
-	hi2c3.Instance = I2C3;
-	hi2c3.Init.ClockSpeed = 100000;
-	hi2c3.Init.DutyCycle = I2C_DUTYCYCLE_2;
-	hi2c3.Init.OwnAddress1 = 0;
-	hi2c3.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-	hi2c3.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-	hi2c3.Init.OwnAddress2 = 0;
-	hi2c3.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-	hi2c3.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-	if (HAL_I2C_Init(&hi2c3) != HAL_OK) {
-		Error_Handler();
-	}
-	/* USER CODE BEGIN I2C3_Init 2 */
+  /* USER CODE END I2C3_Init 1 */
+  hi2c3.Instance = I2C3;
+  hi2c3.Init.ClockSpeed = 100000;
+  hi2c3.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c3.Init.OwnAddress1 = 0;
+  hi2c3.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c3.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c3.Init.OwnAddress2 = 0;
+  hi2c3.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c3.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C3_Init 2 */
 
-	/* USER CODE END I2C3_Init 2 */
-
-}
-
-/**
- * @brief SPDIFRX Initialization Function
- * @param None
- * @retval None
- */
-static void MX_SPDIFRX_Init(void) {
-
-	/* USER CODE BEGIN SPDIFRX_Init 0 */
-
-	/* USER CODE END SPDIFRX_Init 0 */
-
-	/* USER CODE BEGIN SPDIFRX_Init 1 */
-
-	/* USER CODE END SPDIFRX_Init 1 */
-	hspdif.Instance = SPDIFRX;
-	hspdif.Init.InputSelection = SPDIFRX_INPUT_IN2;
-	hspdif.Init.Retries = SPDIFRX_MAXRETRIES_15;
-	hspdif.Init.WaitForActivity = SPDIFRX_WAITFORACTIVITY_ON;
-	hspdif.Init.ChannelSelection = SPDIFRX_CHANNEL_A;
-	hspdif.Init.DataFormat = SPDIFRX_DATAFORMAT_32BITS;
-	hspdif.Init.StereoMode = SPDIFRX_STEREOMODE_ENABLE;
-	hspdif.Init.PreambleTypeMask = SPDIFRX_PREAMBLETYPEMASK_ON;
-	hspdif.Init.ChannelStatusMask = SPDIFRX_CHANNELSTATUS_ON;
-	hspdif.Init.ValidityBitMask = SPDIFRX_VALIDITYMASK_ON;
-	hspdif.Init.ParityErrorMask = SPDIFRX_PARITYERRORMASK_ON;
-	if (HAL_SPDIFRX_Init(&hspdif) != HAL_OK) {
-		Error_Handler();
-	}
-	/* USER CODE BEGIN SPDIFRX_Init 2 */
-
-	/* USER CODE END SPDIFRX_Init 2 */
+  /* USER CODE END I2C3_Init 2 */
 
 }
 
 /**
- * @brief USART6 Initialization Function
- * @param None
- * @retval None
- */
-static void MX_USART6_UART_Init(void) {
+  * @brief SPDIFRX Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPDIFRX_Init(void)
+{
 
-	/* USER CODE BEGIN USART6_Init 0 */
+  /* USER CODE BEGIN SPDIFRX_Init 0 */
 
-	/* USER CODE END USART6_Init 0 */
+  /* USER CODE END SPDIFRX_Init 0 */
 
-	/* USER CODE BEGIN USART6_Init 1 */
+  /* USER CODE BEGIN SPDIFRX_Init 1 */
 
-	/* USER CODE END USART6_Init 1 */
-	huart6.Instance = USART6;
-	huart6.Init.BaudRate = 115200;
-	huart6.Init.WordLength = UART_WORDLENGTH_8B;
-	huart6.Init.StopBits = UART_STOPBITS_1;
-	huart6.Init.Parity = UART_PARITY_NONE;
-	huart6.Init.Mode = UART_MODE_TX_RX;
-	huart6.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-	huart6.Init.OverSampling = UART_OVERSAMPLING_16;
-	if (HAL_UART_Init(&huart6) != HAL_OK) {
-		Error_Handler();
-	}
-	/* USER CODE BEGIN USART6_Init 2 */
+  /* USER CODE END SPDIFRX_Init 1 */
+  hspdif.Instance = SPDIFRX;
+  hspdif.Init.InputSelection = SPDIFRX_INPUT_IN2;
+  hspdif.Init.Retries = SPDIFRX_MAXRETRIES_15;
+  hspdif.Init.WaitForActivity = SPDIFRX_WAITFORACTIVITY_ON;
+  hspdif.Init.ChannelSelection = SPDIFRX_CHANNEL_A;
+  hspdif.Init.DataFormat = SPDIFRX_DATAFORMAT_32BITS;
+  hspdif.Init.StereoMode = SPDIFRX_STEREOMODE_ENABLE;
+  hspdif.Init.PreambleTypeMask = SPDIFRX_PREAMBLETYPEMASK_ON;
+  hspdif.Init.ChannelStatusMask = SPDIFRX_CHANNELSTATUS_ON;
+  hspdif.Init.ValidityBitMask = SPDIFRX_VALIDITYMASK_ON;
+  hspdif.Init.ParityErrorMask = SPDIFRX_PARITYERRORMASK_ON;
+  if (HAL_SPDIFRX_Init(&hspdif) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPDIFRX_Init 2 */
 
-	/* USER CODE END USART6_Init 2 */
-
-}
-
-/**
- * @brief USB_OTG_FS Initialization Function
- * @param None
- * @retval None
- */
-static void MX_USB_OTG_FS_PCD_Init(void) {
-
-	/* USER CODE BEGIN USB_OTG_FS_Init 0 */
-
-	/* USER CODE END USB_OTG_FS_Init 0 */
-
-	/* USER CODE BEGIN USB_OTG_FS_Init 1 */
-
-	/* USER CODE END USB_OTG_FS_Init 1 */
-	hpcd_USB_OTG_FS.Instance = USB_OTG_FS;
-	hpcd_USB_OTG_FS.Init.dev_endpoints = 6;
-	hpcd_USB_OTG_FS.Init.speed = PCD_SPEED_FULL;
-	hpcd_USB_OTG_FS.Init.dma_enable = DISABLE;
-	hpcd_USB_OTG_FS.Init.phy_itface = PCD_PHY_EMBEDDED;
-	hpcd_USB_OTG_FS.Init.Sof_enable = DISABLE;
-	hpcd_USB_OTG_FS.Init.low_power_enable = DISABLE;
-	hpcd_USB_OTG_FS.Init.lpm_enable = DISABLE;
-	hpcd_USB_OTG_FS.Init.vbus_sensing_enable = DISABLE;
-	hpcd_USB_OTG_FS.Init.use_dedicated_ep1 = DISABLE;
-	if (HAL_PCD_Init(&hpcd_USB_OTG_FS) != HAL_OK) {
-		Error_Handler();
-	}
-	/* USER CODE BEGIN USB_OTG_FS_Init 2 */
-
-	/* USER CODE END USB_OTG_FS_Init 2 */
+  /* USER CODE END SPDIFRX_Init 2 */
 
 }
 
 /**
- * Enable DMA controller clock
- */
-static void MX_DMA_Init(void) {
+  * @brief USART6 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART6_UART_Init(void)
+{
 
-	/* DMA controller clock enable */
-	__HAL_RCC_DMA1_CLK_ENABLE();
+  /* USER CODE BEGIN USART6_Init 0 */
 
-	/* DMA interrupt init */
-	/* DMA1_Stream1_IRQn interrupt configuration */
-	HAL_NVIC_SetPriority(DMA1_Stream1_IRQn, 0, 0);
-	HAL_NVIC_EnableIRQ(DMA1_Stream1_IRQn);
+  /* USER CODE END USART6_Init 0 */
+
+  /* USER CODE BEGIN USART6_Init 1 */
+
+  /* USER CODE END USART6_Init 1 */
+  huart6.Instance = USART6;
+  huart6.Init.BaudRate = 115200;
+  huart6.Init.WordLength = UART_WORDLENGTH_8B;
+  huart6.Init.StopBits = UART_STOPBITS_1;
+  huart6.Init.Parity = UART_PARITY_NONE;
+  huart6.Init.Mode = UART_MODE_TX_RX;
+  huart6.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart6.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart6) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART6_Init 2 */
+
+  /* USER CODE END USART6_Init 2 */
 
 }
 
 /**
- * @brief GPIO Initialization Function
- * @param None
- * @retval None
- */
-static void MX_GPIO_Init(void) {
-	GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+  * @brief USB_OTG_FS Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USB_OTG_FS_PCD_Init(void)
+{
 
-	/* GPIO Ports Clock Enable */
-	__HAL_RCC_GPIOH_CLK_ENABLE();
-	__HAL_RCC_GPIOC_CLK_ENABLE();
-	__HAL_RCC_GPIOB_CLK_ENABLE();
-	__HAL_RCC_GPIOA_CLK_ENABLE();
+  /* USER CODE BEGIN USB_OTG_FS_Init 0 */
 
-	/*Configure GPIO pin Output Level */
-	HAL_GPIO_WritePin(GPIOB, LED_G_Pin | LED_Y_Pin | LED_R_Pin, GPIO_PIN_RESET);
+  /* USER CODE END USB_OTG_FS_Init 0 */
 
-	/*Configure GPIO pin : PC0 */
-	GPIO_InitStruct.Pin = GPIO_PIN_0;
-	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-	GPIO_InitStruct.Pull = GPIO_NOPULL;
-	HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+  /* USER CODE BEGIN USB_OTG_FS_Init 1 */
 
-	/*Configure GPIO pins : LED_G_Pin LED_Y_Pin LED_R_Pin */
-	GPIO_InitStruct.Pin = LED_G_Pin | LED_Y_Pin | LED_R_Pin;
-	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-	GPIO_InitStruct.Pull = GPIO_NOPULL;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  /* USER CODE END USB_OTG_FS_Init 1 */
+  hpcd_USB_OTG_FS.Instance = USB_OTG_FS;
+  hpcd_USB_OTG_FS.Init.dev_endpoints = 6;
+  hpcd_USB_OTG_FS.Init.speed = PCD_SPEED_FULL;
+  hpcd_USB_OTG_FS.Init.dma_enable = DISABLE;
+  hpcd_USB_OTG_FS.Init.phy_itface = PCD_PHY_EMBEDDED;
+  hpcd_USB_OTG_FS.Init.Sof_enable = DISABLE;
+  hpcd_USB_OTG_FS.Init.low_power_enable = DISABLE;
+  hpcd_USB_OTG_FS.Init.lpm_enable = DISABLE;
+  hpcd_USB_OTG_FS.Init.vbus_sensing_enable = DISABLE;
+  hpcd_USB_OTG_FS.Init.use_dedicated_ep1 = DISABLE;
+  if (HAL_PCD_Init(&hpcd_USB_OTG_FS) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USB_OTG_FS_Init 2 */
+
+  /* USER CODE END USB_OTG_FS_Init 2 */
+
+}
+
+/**
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void)
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA1_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA1_Stream1_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Stream1_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Stream1_IRQn);
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, LED_G_Pin|LED_Y_Pin|LED_R_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : Btn_Pin */
+  GPIO_InitStruct.Pin = Btn_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(Btn_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : LED_G_Pin LED_Y_Pin LED_R_Pin */
+  GPIO_InitStruct.Pin = LED_G_Pin|LED_Y_Pin|LED_R_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
 
@@ -433,16 +461,17 @@ static void MX_GPIO_Init(void) {
 /* USER CODE END 4 */
 
 /**
- * @brief  This function is executed in case of error occurrence.
- * @retval None
- */
-void Error_Handler(void) {
-	/* USER CODE BEGIN Error_Handler_Debug */
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 	__disable_irq();
 	while (1) {
 	}
-	/* USER CODE END Error_Handler_Debug */
+  /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
